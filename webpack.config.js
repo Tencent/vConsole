7c5365c90c8f94b85339a8c443a20e74a9ca4e63
vicshang <xiaoxiaoxcx@hotmail.com>
2017-10-11 08:41:45 +0800

    修复android4.4，ios8下ajax 一直pending的问题

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   devtool: false,
   entry: {
-    vconsole : './src/vconsole.js'
+    vconsole : ['babel-polyfill','./src/vconsole.js']
   },
   output: {
     path: './dist',
