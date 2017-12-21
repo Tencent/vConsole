6efdf5105dc4fb51e4a220397010ad51d08c2976
zhe-he <hezhe@ihangmei.com>
2017-12-21 17:21:44 +0800

    add reload button

--- src/core/core.js
+++ src/core/core.js
@@ -311,6 +311,11 @@ class VConsole {
       that.hide();
     });
 
+    // reload browser
+    $.bind($.one('.vc-reload', that.$dom), 'click', function() {
+      that.reload();
+    })
+
     // hide console panel when tap background mask
     $.bind($.one('.vc-mask', that.$dom), 'click', function(e) {
       if (e.target != $.one('.vc-mask')) {
@@ -638,6 +643,14 @@ class VConsole {
       $mask.style.display = 'none';
       $panel.style.display = 'none';
     });
+  }
+
+  /** 
+   * reload browser
+   * @public
+   */
+  reload() {
+    window.location.reload();
   }
 
   /**
