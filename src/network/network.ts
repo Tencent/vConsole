cf05b44580c226b4dba09ad9aad26b50ee988f89
Maiz <lariagle@gmail.com>
2021-05-19 21:14:04 +0800

    chore: Use TypeScript.

--- src/network/network.ts
+++ src/network/network.ts
@@ -28,16 +28,15 @@ class VConsoleNetworkRequestItem {
   status: number | string   = 0;
   statusText: string        = '';
   readyState: XMLHttpRequest['readyState'] = 0;
-  header: { [key: string]: string } = null; // response header
+  header: { [key: string]: string };
+  requestHeader: HeadersInit;
   responseType: XMLHttpRequest['responseType'];
-  requestType: 'xhr' | 'fetch' | 'ping';
-  requestHeader: HeadersInit = null;
   response: any;
   startTime: number         = 0;
   endTime: number           = 0;
   costTime: number          = 0;
-  getData: { [key: string]: string } = null;
-  postData: { [key: string]: string } | string = null;
+  getData: { [key: string]: string };
+  postData: { [key: string]: string } | '[object Object]';
   actived: boolean = false;
 
   constructor(id: string) {
@@ -52,11 +51,9 @@ class VConsoleNetworkTab extends VConsolePlugin {
   private domList: { [id: string]: Element } = {};
   private isShow: boolean = false;
   private isInBottom: boolean = true; // whether the panel is in the bottom
-  private _xhrOpen: XMLHttpRequest['open'] = undefined; // the origin function
-  private _xhrSend: XMLHttpRequest['send'] = undefined;
-  private _xhrSetRequestHeader: XMLHttpRequest['setRequestHeader'] = undefined;
-  private _fetch: WindowOrWorkerGlobalScope['fetch'] = undefined;
-  private _sendBeacon: Navigator['sendBeacon'] = undefined;
+  private _open: any = undefined; // the origin function
+  private _send: any = undefined;
+  private _setRequestHeader: any = undefined;
 
   constructor(...args) {
     super(...args);
@@ -121,20 +118,12 @@ class VConsoleNetworkTab extends VConsolePlugin {
   onRemove() {
     // recover original functions
     if (window.XMLHttpRequest) {
-      window.XMLHttpRequest.prototype.open = this._xhrOpen;
-      window.XMLHttpRequest.prototype.send = this._xhrSend;
-      window.XMLHttpRequest.prototype.setRequestHeader = this._xhrSetRequestHeader;
-      this._xhrOpen = undefined;
-      this._xhrSend = undefined;
-      this._xhrSetRequestHeader = undefined;
-    }
-    if (window.fetch) {
-      window.fetch = this._fetch;
-      this._fetch = undefined;
-    }
-    if (window.navigator.sendBeacon) {
-      window.navigator.sendBeacon = this._sendBeacon;
-      this._sendBeacon = undefined;
+      window.XMLHttpRequest.prototype.open = this._open;
+      window.XMLHttpRequest.prototype.send = this._send;
+      window.XMLHttpRequest.prototype.setRequestHeader = this._setRequestHeader;
+      this._open = undefined;
+      this._send = undefined;
+      this._setRequestHeader = undefined
     }
   }
 
@@ -181,7 +170,7 @@ class VConsoleNetworkTab extends VConsolePlugin {
     this.renderHeader();
   }
 
-  private renderHeader() {
+  renderHeader() {
     const count = Object.keys(this.reqList).length,
         $header = $.render(tplHeader, {count: count}),
         $logbox = $.one('.vc-log', this.$tabbox);
@@ -199,7 +188,7 @@ class VConsoleNetworkTab extends VConsolePlugin {
    * add or update a request item by request ID
    * @private
    */
-  private updateRequest(id: string, data: VConsoleNetworkRequestItem | Object) {
+  updateRequest(id: string, data: VConsoleNetworkRequestItem | Object) {
     // see whether add new item into list
     const preCount = Object.keys(this.reqList).length;
 
@@ -214,6 +203,63 @@ class VConsoleNetworkTab extends VConsolePlugin {
     if (!this.isReady) {
       return;
     }
+
+    // update dom
+    // let domData = {
+    //   id: id,
+    //   name: item.name,
+    //   url: item.url,
+    //   status: item.status,
+    //   method: item.method || '-',
+    //   costTime: item.costTime>0 ? item.costTime+'ms' : '-',
+    //   header: item.header || null,
+    //   requestHeader: item.requestHeader || null,
+    //   getData: item.getData || null,
+    //   postData: item.postData || null,
+    //   response: null,
+    //   actived: !!item.actived
+    // };
+    // switch (item.responseType) {
+    //   case '':
+    //   case 'text':
+    //     // try to parse JSON
+    //     if (tool.isString(item.response)) {
+    //       try {
+    //         domData.response = JSON.parse(item.response);
+    //         domData.response = JSON.stringify(domData.response, null, 1);
+    //         domData.response = domData.response;
+    //       } catch (e) {
+    //         // not a JSON string
+    //         domData.response = item.response;
+    //       }
+    //     } else if (typeof item.response != 'undefined') {
+    //       domData.response = Object.prototype.toString.call(item.response);
+    //     }
+    //     break;
+    //   case 'json':
+    //     if (typeof item.response != 'undefined') {
+    //       domData.response = JSON.stringify(item.response, null, 1);
+    //       domData.response = domData.response;
+    //     }
+    //     break;
+    //   case 'blob':
+    //   case 'document':
+    //   case 'arraybuffer':
+    //   default:
+    //     if (typeof item.response != 'undefined') {
+    //       domData.response = Object.prototype.toString.call(item.response);
+    //     }
+    //     break;
+    // }
+    // if (item.readyState == 0 || item.readyState == 1) {
+    //   domData.status = 'Pending';
+    // } else if (item.readyState == 2 || item.readyState == 3) {
+    //   domData.status = 'Loading';
+    // } else if (item.readyState == 4) {
+    //   // do nothing
+    // } else {
+    //   domData.status = 'Unknown';
+    // }
     const $new = $.render(tplItem, item),
           $old = this.domList[id];
     if (item.status >= 400) {
@@ -242,7 +288,7 @@ class VConsoleNetworkTab extends VConsolePlugin {
    * mock XMLHttpRequest
    * @private
    */
-  private mockXHR() {
+  mockXHR() {
     const _XMLHttpRequest = window.XMLHttpRequest;
     if (!_XMLHttpRequest) { return; }
 
@@ -250,9 +296,9 @@ class VConsoleNetworkTab extends VConsolePlugin {
     const _open = window.XMLHttpRequest.prototype.open,
           _send = window.XMLHttpRequest.prototype.send,
           _setRequestHeader = window.XMLHttpRequest.prototype.setRequestHeader;
-    that._xhrOpen = _open;
-    that._xhrSend = _send;
-    that._xhrSetRequestHeader = _setRequestHeader;
+    that._open = _open;
+    that._send = _send;
+    that._setRequestHeader = _setRequestHeader;
 
     // mock open()
     window.XMLHttpRequest.prototype.open = function() {
@@ -277,7 +323,6 @@ class VConsoleNetworkTab extends VConsolePlugin {
         // update status
         item.readyState = XMLReq.readyState;
         item.responseType = XMLReq.responseType;
-        item.requestType = 'xhr';
 
         // update data by readyState
         switch (XMLReq.readyState) {
@@ -459,43 +504,40 @@ class VConsoleNetworkTab extends VConsolePlugin {
    * mock fetch request
    * @private
    */
-  private mockFetch() {
+  mockFetch() {
     const _fetch = window.fetch;
     if (!_fetch) { return; }
     const that = this;
-    this._fetch = _fetch;
 
-    window.fetch = (input: RequestInfo, init?: RequestInit) => {
+    const prevFetch = (input: RequestInfo, init?: RequestInit) => {
       const id = that.getUniqueID();
       const item = new VConsoleNetworkRequestItem(id);
       that.reqList[id] = item;
-      let url: URL,
+      let query = [],
+          url = '',
           method = 'GET',
           requestHeader: HeadersInit = null;
-      let _fetchReponse: Response;
 
       // handle `input` content
       if (tool.isString(input)) { // when `input` is a string
         method = init?.method || 'GET';
-        url = that.getURL(<string>input);
+        url = <string>input;
         requestHeader = init?.headers || null;
       } else { // when `input` is a `Request` object
         method = (<Request>input).method || 'GET';
-        url = that.getURL((<Request>input).url);
+        url = (<Request>input).url;
         requestHeader = (<Request>input).headers;
       }
+      query = url.split('?');
 
       item.id = id;
       item.method = method;
-      item.requestType = 'fetch';
       item.requestHeader = requestHeader;
-      item.url = url.toString();
-      item.name = (url.pathname.split('/').pop() || '') + url.search;
+      item.url = url;
+      item.name = query.shift() || '';
+      item.name = item.name.replace(new RegExp('[/]*$'), '').split('/').pop() || '';
       item.status = 0;
       item.statusText = 'Pending';
-      if (!item.startTime) { // UNSENT
-        item.startTime = (+new Date());
-      }
 
       if (Object.prototype.toString.call(requestHeader) === '[object Headers]') {
         item.requestHeader = {};
@@ -508,172 +550,152 @@ class VConsoleNetworkTab extends VConsolePlugin {
       }
 
       // save GET data
-      if (url.search) {
+      if (query.length > 0) {
+        item.name += '?' + query;
         item.getData = {};
-        url.searchParams.forEach((value, key) => {
-          item.getData[key] = value;
-        });
+        query = query.join('?').split('&'); // join() => 'b=c&d=?e', split() => ['b=c', 'd=?e']
+        for (let q of query) {
+          q = q.split('=');
+          item.getData[ q[0] ] = q[1];
+        }
       }
 
       // save POST data
       if (item.method === 'POST') {
         if (tool.isString(input)) { // when `input` is a string
-          item.postData = that.getFormattedBody(init.body);
+          if (tool.isString(init?.body)) {
+            const arr = (<string>init.body).split('&');
+            item.postData = {};
+            for (let q of arr) {
+              const kv = q.split('=');
+              item.postData[ kv[0] ] = kv[1];
+            }
+          } else if (tool.isPlainObject(init?.body)) {
+            item.postData = (<{}>init?.body);
+          } else {
+            item.postData = '[object Object]';
+          }
         } else { // when `input` is a `Request` object
           // cannot get real type of request's body, so just display "[object Object]"
           item.postData = '[object Object]';
         }
       }
 
-      return _fetch(url.toString(), init).then((response) => {
-        _fetchReponse = response;
-
-        item.endTime = +new Date();
-        item.costTime = item.endTime - (item.startTime || item.endTime);
-        item.status = response.status;
-        item.statusText = String(response.status);
-
-        item.header = {};
-        // @ts-ignore
-        for (let pair of response.headers.entries()) {
-          item.header[pair[0]] = pair[1];
-        }
-        item.readyState = 4;
-
-        // parse response body by Content-Type
-        const contentType = response.headers.get('content-type');
-        if (contentType.includes('application/json')) {
-          item.responseType = 'json';
-          return response.clone().text();
-        } else if (contentType.includes('text/html')) {
-          item.responseType = 'text';
-          return response.clone().text();
-        } else {
-          item.responseType = '';
-          return '[object Object]';
-        }
-
-      }).then((responseBody) => {
-        // save response body
-        switch (item.responseType) {
-          case 'json':
-            try {
-              // try to parse response as JSON
-              item.response = JSON.parse(responseBody);
-              item.response = JSON.stringify(item.response, null, 1);
-            } catch (e) {
-              // not real JSON, use 'text' as default type
-              item.response = responseBody;
-              item.responseType = 'text';
+      // UNSENT
+      if (!item.startTime) {
+        item.startTime = (+new Date());
+      }
+      return _fetch(url, init).then((response) => {
+        response
+          .clone()
+          .text()
+          .then((text) => {
+            const contentType = response.headers.get('content-type');
+            // use 'text' as default type in case of contentType is json but response is not real JSON
+            let itemResponse = text;
+            let itemResponseType: XMLHttpRequest['responseType'] = ''; // use XHR responseType
+            if (contentType.includes('application/json')) {
+              try {
+                itemResponse = JSON.parse(text);
+                itemResponseType = 'json';
+              } catch (e) {}
+            } else if (contentType.includes('text/html')) {
+              itemResponseType = 'text';
             }
-            break;
+            item.response = itemResponse;
+            item.responseType = itemResponseType;
 
-          case 'text':
-          default:
-            item.response = responseBody;
-            break;
-        }
+            item.endTime = +new Date();
+            item.costTime = item.endTime - (item.startTime || item.endTime);
+            
+            item.status = response.status;
+            item.statusText = String(response.status);
 
-        return _fetchReponse;
-      }).finally(() => {
-        _fetchReponse = undefined;
-        that.updateRequest(id, item);
-      });
-    };
+            item.header = {};
+            // @ts-ignore
+            for (let pair of response.headers.entries()) {
+              item.header[pair[0]] = pair[1];
+            }
+            item.readyState = 4;
+
+            return itemResponse;
+          }).finally(() => {
+            that.updateRequest(id, item);
+          });
+        return response;
+      })
+    }
+    window.fetch = prevFetch;
   }
 
   /**
    * mock navigator.sendBeacon
    * @private
    */
-  private mockSendBeacon() {
-    const _sendBeacon = window.navigator.sendBeacon;
-    if (!_sendBeacon) { return; }
-    const that = this;
-    this._sendBeacon = _sendBeacon;
-
+  mockSendBeacon() {
     // https://fetch.spec.whatwg.org/#concept-bodyinit-extract
-    const getContentType = (data?: BodyInit) => {
+    const getContentType = (data) => {
       if (data instanceof Blob) { return data.type; }
       if (data instanceof FormData) { return 'multipart/form-data'; }
       if (data instanceof URLSearchParams) { return 'application/x-www-form-urlencoded;charset=UTF-8'; }
       return 'text/plain;charset=UTF-8';
-    };
+    }
 
-    window.navigator.sendBeacon = (urlString: string, data?: BodyInit) => {
+    const _sendBeacon = window.navigator.sendBeacon;
+    if (!_sendBeacon) { return; }
+    const that = this;
+
+    window.navigator.sendBeacon = (url, data) => {
       const id = that.getUniqueID();
       const item = new VConsoleNetworkRequestItem(id);
       that.reqList[id] = item;
 
-      const url = that.getURL(urlString);
+      let query = url.split('?');
       item.id = id;
       item.method = 'POST';
-      item.url = urlString;
-      item.name = (url.pathname.split('/').pop() || '') + url.search;
-      item.requestType = 'ping';
+      item.url = url;
+      item.name = query.shift() || '';
+      item.name = item.name.replace(new RegExp('[/]*$'), '').split('/').pop() || '';
       item.requestHeader = { 'Content-Type': getContentType(data) };
       item.status = 0;
       item.statusText = 'Pending';
-      
-      if (url.search) {
+
+      if (query.length > 0) {
+        item.name += '?' + query;
         item.getData = {};
-        url.searchParams.forEach((value, key) => {
-          item.getData[key] = value;
-        });
+        query = query.join('?').split('&'); // join() => 'b=c&d=?e', split() => ['b=c', 'd=?e']
+        for (const q of query) {
+          const kv = q.split('=');
+          item.getData[ kv[0] ] = kv[1];
+        }
       }
-      item.postData = that.getFormattedBody(data);
+
+      if (tool.isString(data)) {
+        let arr = (<string>data).split('&');
+        item.postData = {};
+        for (let q of arr) {
+          const kv = q.split('=');
+          item.postData[ kv[0] ] = kv[1];
+        }
+      } else {
+        item.postData = '[object Object]';
+      }
 
       if (!item.startTime) {
         item.startTime = (+new Date());
       }
 
-      const isSuccess = _sendBeacon.call(window.navigator, urlString, data);
+      const isSuccess = _sendBeacon.call(window.navigator, url, data);
       if (isSuccess) {
         item.endTime = +new Date();
         item.costTime = item.endTime - (item.startTime || item.endTime);
-        item.status = 0;
-        item.statusText = 'Sent';
+        item.status = 200;
+        item.statusText = '200';
         item.readyState = 4;
-      } else {
-        item.status = 500;
-        item.statusText = 'Unknown';
       }
       that.updateRequest(id, item);
       return isSuccess;
-    };
-  }
-
-  private getFormattedBody(body?: BodyInit) {
-    if (!body) { return null; }
-    let ret: string | { [key: string]: string } = null;
-    const type = tool.getPrototypeName(body);
-    switch (type) {
-      case 'String':
-        try {
-          // try to parse as JSON
-          ret = JSON.parse(<string>body);
-        } catch (e) {
-          // not a json, return original string
-          ret = <string>body;
-        }
-        break;
-
-      case 'URLSearchParams':
-        ret = {};
-        (<URLSearchParams>body).forEach((value, key) => {
-          ret[key] = value;
-        });
-        break;
-
-      default:
-        ret = `[object ${type}]`;
-        break;
     }
-    return ret;
-  }
-
-  private getURL(urlString: string) {
-    return new URL(urlString, urlString.includes('http') ? undefined : window.location.href);
   }
 
   /**
@@ -681,7 +703,7 @@ class VConsoleNetworkTab extends VConsolePlugin {
    * @private
    * @return string
    */
-  private getUniqueID() {
+  getUniqueID() {
     const id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       const r = Math.random() * 16 | 0;
       const v = c == 'x' ? r : (r & 0x3 | 0x8);
