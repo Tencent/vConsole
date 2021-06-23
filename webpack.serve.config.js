52a2f2a7d558a2410fea72ce2652636370de06f6
Maiz <lariagle@gmail.com>
2021-06-23 21:06:05 +0800

    chore: Add  to Webpack to avoid compatibility issues. (issue #404)

---
+++ webpack.serve.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.config.js');
+
+const contentBase = path.join(__dirname, '/');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 9191,
+    disableHostCheck: true,
+    contentBase: contentBase,
+    openPage: 'dev/index.html',
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true,
+    open: true,
+    hot: true,
+    inline: true,
+    before(app) {
+      app.all('*', (req, res) => {
+        const delay = req.query.t || Math.ceil(Math.random() * 100);
+        setTimeout(() => {
+          res.status(req.query.s || 200);
+          const filePath = path.join(contentBase, req.path);
+          try {
+            fs.accessSync(filePath, fs.constants.F_OK);
+            // res.send(fs.readFileSync(filePath));
+            res.sendFile(filePath);
+          } catch (e) {
+            res.end();
+          }
+          // console.log(req.query);
+        }, delay);
+      });
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
